fc035f68ff92cbb52d2c64116f4124246aa3f6e0
AneeshRijhwani25 <7851950798aneesh@gmail.com>
2024-08-06 08:54:24 +0530

    Intitial Setup done

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config({ path: ".env" });
+const app = express();
+
+app.use(
+    cors({
+        origin: "*",
+        credentials: true,
+    })
+);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    console.log("Welcome to Zoomcars");
+});
